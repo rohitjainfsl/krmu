039de38129190ac7efe1400cf5ae479a0043f588
rohitjainfslm <rohit@fullstacklearning.com>
2026-09-23 15:13:46 +0530

    JS Loops

---
+++ section a/loops.js
@@ -0,0 +1,23 @@
+// let x = 0;
+// for (let i = 0; i <= 5; i++) {
+//   for (let j = 0; j < 5; j++) {
+//     x = x + i;
+//   }
+// }
+// console.log(x);
+
+//Post: increment, decrement
+//Pre: increment, decrement
+
+// let a = 0;
+// a++;
+// a--;
+// ++a;
+// --a;
+// console.log(a);
+
+let x = 1;
+let y = ++x + 1; 
+let z = y-- + 1; 
+console.log(x++ + y++ + z++);
+console.log(x + y + z);
